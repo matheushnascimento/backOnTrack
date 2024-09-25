7f370aa1f14f70dfbd452b4b2391070679f11de2
Matheus Nascimento <89102783+matheushnascimento@users.noreply.github.com>
2024-09-25 11:16:14 +0000

    feaat: create `test` migration file

--- infra/migrations/1726579055701_test.js
+++ infra/migrations/1726579055701_test.js
@@ -1,18 +1,5 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-exports.shorthands = undefined;
+exports.shorthand = undefined;
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.up = (pgm) => {};
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.down = (pgm) => {};
