e12d25fa2adfa78990e00cc1555e01769ded5b7a
matheushnascimento <matheus.mhddn@gmail.com>
2024-11-26 12:49:08 -0300

    feat: create `test orchestrator`

---
+++ tests/orchestrators.js
@@ -0,0 +1,30 @@
+import retry from "async-retry";
+import database from "infra/database";
+async function waitForAllServices() {
+  waitForWebServer();
+}
+
+async function clearDatabase() {
+  await database.query("DROP schema public cascade; create schema public");
+}
+
+async function waitForWebServer() {
+  return retry(fetchStatusPage, {
+    retries: 100,
+    maxTimeout: 1000,
+  });
+
+  async function fetchStatusPage() {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    if (!response.ok) {
+      throw new Error();
+    }
+  }
+}
+
+const orchestrator = {
+  waitForAllServices,
+  clearDatabase,
+};
+
+export default orchestrator;
