2afa0e0541d4ef99a00aea177477940749091d7a
matheushnascimento <matheus.mhddn@gmail.com>
2025-08-08 11:14:55 -0300

    feat: create Colors file

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+export const Colors = {
+  primary: "#2E5A88",
+  secondary: "#4CAF50",
+  light: {
+    background: "#F8F9FA",
+    text: "#333333",
+    outerText: "#F8F9FA",
+  },
+  dark: {
+    background: "#333333",
+    text: "#F8F9FA",
+    outerText: "#333333",
+  },
+};
