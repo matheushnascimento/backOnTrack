163bfea771d91a4445b44b4b6fc18c374f3b510b
matheushnascimento <matheus.mhddn@gmail.com>
2025-08-08 17:17:00 -0300

    feat: update home page under construction

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,17 @@
-import { Text, View, useColorScheme } from "react-native";
+import { Image, View, useColorScheme } from "react-native";
 import { Colors } from "../constants/Colors";
+import image from "../assets/trecho-em-obras.png";
 
 export default function Home() {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
   return (
     <>
-      <View style={{ flex: 1, backgroundColor: theme.background }}>
-        <Text
-          style={{ color: theme.text }}
-          className="text-[4rem] font-bold text-center"
-        >
-          Esse pedacinho de tecnologia está em construção!
-        </Text>
+      <View
+        className="flex-1 items-center"
+        style={{ backgroundColor: theme.background }}
+      >
+        <Image source={image} />
       </View>
     </>
   );
